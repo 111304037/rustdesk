ce0239750d6d77036b563af76963a8a326e10fa0
mengxingquan <mengxingquan@corp.netease.com>
2024-08-18 00:13:17 +0800

    构建成功

--- build_win.bat
+++ build_win.bat
@@ -10,7 +10,11 @@ set PY_ROOT=E:\MyFiles\Python\Python38
 set PY_PIP=%PY_ROOT%\Scripts
 set PY_LIBS=%PY_ROOT%\Lib\site-packages
 
-set PATH=%LD_LIBRARY_PATH%;%PY_ROOT%;%PY_PIP%;%PY_LIBS%;%VCPKG_ROOT%;%LLVM_ROOT%;%PATH%
+set env_nasm=G:\Mxq_Share\rustdesk\deps\vcpkg\vcpkg\downloads\tools\nasm\nasm-2.15.05
+set env_perl=G:\Mxq_Share\rustdesk\deps\vcpkg\vcpkg\downloads\tools\perl\5.32.1.1\perl\bin
+
+set env_rustdesk_build=%env_nasm%;%env_perl%;
+set PATH=%env_rustdesk_build%;%LD_LIBRARY_PATH%;%PY_ROOT%;%PY_PIP%;%PY_LIBS%;%VCPKG_ROOT%;%LLVM_ROOT%;%PATH%;
 
 @REM set HTTP_PROXY=127.0.0.1:8899
 @REM set HTTPS_PROXY=127.0.0.1:8899
@@ -18,10 +22,13 @@ set PATH=%LD_LIBRARY_PATH%;%PY_ROOT%;%PY_PIP%;%PY_LIBS%;%VCPKG_ROOT%;%LLVM_ROOT%
 
 @REM first init vcpkg
 @REM bootstrap-vcpkg.bat
-@REM vcpkg install libvpx:x64-windows-static libyuv:x64-windows-static opus:x64-windows-static
+@REM vcpkg install libvpx:x64-windows-static
+@REM vcpkg install libyuv:x64-windows-static
+@REM vcpkg install opus:x64-windows-static
+@REM vcpkg install aom:x64-windows-static
 @REM vcpkg_cli probe libvpx
 @REM vcpkg list
-cargo build
+@REM cargo build
 ::cargo run
 ::cargo clean
 ::cargo cache
