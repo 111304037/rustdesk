17e9235332dfa4650c079493f14dbfcf253cbecd
mengxingquan <mengxingquan@163.com>
2025-04-21 03:19:30 +0800

    android build

--- build_android.bat
+++ build_android.bat
@@ -3,6 +3,9 @@
 
 @REM python
 set PythonLocation=E:\MyFiles\Python\Python38
+if not exist "%PythonLocation%" (
+	set PythonLocation=D:\App\Python310
+)
 set PY_PIP=%PY_ROOT%\Scripts
 set PY_DLLs=%PY_ROOT%\DLLs
 set PY_LIBS=%PY_ROOT%\Lib\site-packages
@@ -12,34 +15,66 @@ set PATH=%PYTHONPATH%;%PATH%
 
 @REM rustdesk
 set env_toolchain=%~dp0\deps\toolchain-windows\bin
-set VCPKG_ROOT=%~dp0\deps\vcpkg\vcpkg
-set VPX_LIB_DIR=%VCPKG_ROOT%\installed\arm64-windows-static
-set LD_LIBRARY_PATH=%VCPKG_ROOT%\installed\arm64-windows-static
 
-set pkg_vpx=%VCPKG_ROOT%\packages\libvpx_arm64-windows-static\lib\pkgconfig
-set PKG_CONFIG_PATH=%VCPKG_ROOT%/installed/arm64-windows-static/lib/pkgconfig;%pkg_vpx%;%PKG_CONFIG_PAT%
+@REM vcpkg
+set VCPKG_ROOT=%~dp0deps\vcpkg\vcpkg
+set pkg_vpx=%VCPKG_ROOT%\packages\libvpx_arm64-static\lib\pkgconfig
+set PKG_CONFIG_PATH=%VCPKG_ROOT%/installed/arm64/lib/pkgconfig;%pkg_vpx%;%PKG_CONFIG_PAT%
+set LD_LIBRARY_PATH=%VCPKG_ROOT%\installed\arm64-static\lib
+set VPX_LIB_DIR=%VCPKG_ROOT%\installed\arm64-static\lib
+set VPX_INCLUDE_DIR=%VCPKG_ROOT%\installed\arm64-static\include
+
 
 set env_nasm=%VCPKG_ROOT%\downloads\tools\nasm\nasm-2.15.05
 set env_perl=%VCPKG_ROOT%\downloads\tools\perl\5.32.1.1\perl\bin
-set env_cmake=%VCPKG_ROOT%\downloads\tools\cmake-3.29.2-windows\cmake-3.29.2-windows-i386\bin
+@REM set env_cmake=%VCPKG_ROOT%\downloads\tools\cmake-3.30.1-windows\cmake-3.30.1-windows-i386\bin
 set env_vckpg=%env_nasm%;%env_perl%;%env_cmake%;
 
 set VCPKGRS_DYNAMIC=1
-set LLVM_ROOT=D:\Program Files\LLVM
+set LLVM_ROOT=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\Llvm\x64
 set LIBCLANG_PATH=%LLVM_ROOT%\bin
-set env_rustdesk_build=%env_vckpg%;%env_toolchain%;%PKG_CONFIG_PATH%;
+set env_rustdesk_build=%env_vckpg%;%env_toolchain%;%PKG_CONFIG_PATH%;%LIBCLANG_PATH%;
 
 set PATH=%env_rustdesk_build%;%LD_LIBRARY_PATH%;%VCPKG_ROOT%;%LLVM_ROOT%;%PATH%;
 
+@REM set HTTP_PROXY=http://10.227.199.162:808
+@REM set HTTPS_PROXY=http://10.227.199.162:808
+git config --global http.proxy http://10.227.199.162:808
+git config --global https.proxy http://10.227.199.162:808
 
+@REM git config --global --unset http.proxy
+@REM git config --global --unset https.proxy
+set VCPKG_KEEP_ENV_VARS=MSYS2_MIRROR
+set MSYS2_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/msys2
+set VCPKG_DEFAULT_TRIPLET_OVERRIDE=arm64-windows-tuna
 
+clang --version
 
+@REM first init vcpkg
+@REM bootstrap-vcpkg.bat
+@REM vcpkg install libvpx:x64-windows-static
+@REM vcpkg install libyuv:x64-windows-static
+@REM vcpkg install opus:x64-windows-static
+@REM vcpkg install aom:x64-windows-static
+@REM vcpkg_cli probe libvpx
+@REM vcpkg list
+@REM cargo run
+::cargo clean
+::cargo cache
+
+set CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG=true
+set RUST_BACKTRACE=full
+@REM cargo clean
+@REM cargo update
+@REM cargo build --target aarch64-linux-android
+
+python -V
 
 
 
 set ANDROID_SDK=B:\Android\sdk
 if not exist "%ANDROID_SDK%" (
-	set ANDROID_SDK=E:\App\Android\sdk
+	set ANDROID_SDK=D:\App\Android\sdk
 )
 
 set ANDROID_HOME=%ANDROID_SDK%
@@ -65,10 +100,11 @@ cargo add opus
 @REM cargo add aom
 cargo add sodium
 
+@REM 为 ARM64 设备添加目标支持
 rustup target add aarch64-linux-android
 cargo install cargo-ndk
 @REM VCPKG_ROOT=$HOME/vcpkg ANDROID_NDK_HOME=$HOME/android-ndk-r23c flutter/ndk_arm64.sh
-cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec
+cargo ndk --platform 25 --target aarch64-linux-android build --release --features flutter,hwcodec
 cmd /k
 
 
