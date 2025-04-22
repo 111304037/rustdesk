60eea5a73dd3ecff6f1931fff8b370898ce47f7e
111304037 <744722817@qq.com>
2025-04-22 09:51:13 +0800

    mac vkpkg install

--- build_mac.sh
+++ build_mac.sh
@@ -5,12 +5,14 @@ echo "dirname=$0"
 RootDir=$(cd `dirname $0`; pwd)
 echo "RootDir=$RootDir"
 cd ${RootDir}
+pwd
 
 source ${RootDir}/env_mac.sh
 source ~/.bashrc
 echo "PATH=$PATH"
 echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 
+echo "VPX_INCLUDE_DIR=$VPX_INCLUDE_DIR"
 # chmod +x ${VCPKG_ROOT}/bootstrap-vcpkg.sh
 # ${VCPKG_ROOT}/bootstrap-vcpkg.sh -disableMetrics
 # ${VCPKG_ROOT}/vcpkg install libvpx libyuv opus aom
@@ -23,21 +25,19 @@ echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 # python3 -m pip install --upgrade pip
 # pip3 install -r requirements.txt
 
-# wget https://github.com/c-smile/sciter-sdk/raw/master/bin.osx/libsciter.dylib
-# brew tap leoafarias/fvm
-# brew install fvm cocoapods
-# fvm global 3.16.9
-# cargo install flutter_rust_bridge_codegen --version "1.80.1" --features "uuid"
+cd ${RootDir}
+pwd
 
+# wget https://github.com/c-smile/sciter-sdk/raw/master/bin.osx/libsciter.dylib
 # #install flutter
-# brew tap leoafarias/fvm
-# brew install fvm cocoapods
-# fvm global 3.16.9
+brew tap leoafarias/fvm
+brew install fvm cocoapods
+fvm global 3.16.9
 
 # flutter --disable-analytics
 # dart --disable-analytics
 # flutter doctor -v
-# cargo install flutter_rust_bridge_codegen --version "1.80.1" --features "uuid"
+cargo install flutter_rust_bridge_codegen --version "1.80.1" --features "uuid"
 
 
 # #Sciter版本
@@ -52,5 +52,9 @@ deps/vcpkg/vcpkg/bootstrap-vcpkg.sh
 vcpkg --version
 vcpkg help triplet | grep -i osx
 # 安装vcpkg.json
-vcpkg install
-cargo run
+vcpkg install --triplet=$VCPKG_TARGET_TRIPLET
+# cargo run
+python3 build.py --flutter --hwcodec --unix-file-copy-paste
+
+
+exec $SHELL
--- env_mac.sh
+++ env_mac.sh
@@ -1,33 +1,58 @@
 #!/bin/sh -e
 
-
 echo "dirname=$0"
 RootDir=$(cd `dirname $0`; pwd)
 echo "RootDir=$RootDir"
 cd ${RootDir}
 
+# Architecture
 export CARGO_CFG_TARGET_ARCH=aarch64
 
+# Cargo
 export CARGO_ROOT=/Users/game-netease/.cargo/bin
 
-#vcpkg
+# VCPKG Configuration
 export VCPKG_ROOT=${RootDir}/deps/vcpkg/vcpkg
-# export pkg_vpx=${VCPKG_ROOT}/packages/libvpx_x64-osx/lib/pkgconfig
-export pkg_vpx=/opt/homebrew/Cellar/libvpx/1.15.1/lib/pkgconfig
-export PKG_CONFIG_PATH=${RootDir}/deps/vcpkg/vcpkg/installed/x64-osx/lib/pkgconfig:$pkg_vpx:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=${VCPKG_ROOT}\installed\x64-osx\lib
-export VPX_LIB_DIR=${VCPKG_ROOT}\installed\x64-osx\lib
-export VPX_INCLUDE_DIR=${VCPKG_ROOT}\installed\x64-osx\include
-export VCPKG_DEFAULT_TRIPLET=arm64-osx-dynamic
+export VCPKG_DEFAULT_TRIPLET=arm64-osx
+export VCPKG_TARGET_TRIPLET=$VCPKG_DEFAULT_TRIPLET
+export VCPKG_ARCH=arm64-osx
 export VCPKGRS_DYNAMIC=1
 
-# path
-export PATH=$HOME/fvm/default/bin:$PATH
-export PATH=${PKG_CONFIG_PATH}:${CARGO_ROOT}:$HOME/fvm/default/bin:${VCPKG_ROOT}:$PATH
+# # arm64-osx-dynamic.cmake
+# set(VCPKG_TARGET_ARCHITECTURE arm64)
+# set(VCPKG_CRT_LINKAGE dynamic)
+# set(VCPKG_LIBRARY_LINKAGE dynamic)  # 关键配置项
+# set(VCPKG_OSX_ARCHITECTURES arm64)
 
+# VPX Configuration
+export VPX_INCLUDE_DIR=${RootDir}/vcpkg_installed/$VCPKG_ARCH/include
+export VPX_LIB_DIR=${RootDir}/vcpkg_installed/$VCPKG_ARCH//lib
+export LIBVPX_INCLUDE_DIR=$LIBVPX_INCLUDE_DIR
+export LIBVPX_LIB_DIR=$VPX_LIB_DIR
+
+# OPUS Configuration
+export OPUS_INCLUDE_DIR=${RootDir}/vcpkg_installed/$VCPKG_ARCH/include
+export OPUS_LIB_DIR=${RootDir}/vcpkg_installed/$VCPKG_ARCH/lib
+
+# Include Paths
+export CPATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/include:$CPATH
+export C_INCLUDE_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/include:$CPLUS_INCLUDE_PATH
+# Library Paths 
+export LIBRARY_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/lib:$DYLD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${RootDir}/vcpkg_installed/$VCPKG_ARCH/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# PATH
+export PATH=$HOME/fvm/default/bin:$PATH
+export PATH=${PKG_CONFIG_PATH}:${VCPKG_ROOT}:$PATH
+
+# Debug settings
 export CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG=true
 export RUST_BACKTRACE=full
 
+# Source cargo environment
 source $HOME/.cargo/env
 source ~/.bashrc
 echo "PATH=$PATH"
